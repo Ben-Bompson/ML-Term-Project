95afc3a162c541a4cec206e12c0df6db87c38739
victorgooge <129326748+victorgooge@users.noreply.github.com>
2025-04-06 17:38:39 -0400

    Test Commit

--- pc_builder.py
+++ pc_builder.py
@@ -5,7 +5,7 @@ budget = 1500
 def find_gpu():
     for i in gpu_df.iloc:
         if i["Price"] < budget/2:
-            return i["GPU Name"]
+            return f'{i["GPU Name"]} ~ ${i["Price"]}'
 
 gpu = find_gpu()
 print(gpu)
